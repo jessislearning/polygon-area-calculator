192c84bfd334015ffef9639f860020da73bab3e5
Jessica A <161026755+jessislearning@users.noreply.github.com>
2024-08-19 17:49:56 +0800

    added basic metho

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,40 @@
 class Rectangle:
+    
+    def __init__(self, width, height):
+        pass
 
+    def set_width(self):
+        pass
 
+    def set_height(self):
+        pass
+    
+    def get_area(self):
+        pass
+        #area = width*height
+        #return area
+    
+    def get_perimeter(self):
+        #perimeter = 2*width + 2*height
+        pass
+        #return perimeter
+    
+    def get_diagonal(self):
+        #diagonal = ((width ** 2 + height ** 2) ** .5)
+        pass
+        #return diagonal
+    
+    def get_picture(self):
+        pass
+
+    def get_amount_inside(self):
+        pass
 
 
 class Square:
+    
+    def __init__(self):
+        pass
+
+    def set_side(self):
+        pass
