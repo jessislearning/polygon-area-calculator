13302f8eabb8945214f57abd1e1592d9197b2ea0
Jessica A <161026755+jessislearning@users.noreply.github.com>
2024-08-21 10:20:10 +0800

    methods for Rectangle class
    - __init__
    - __str__
    - set_width, set_height, get_perimeter, get_area, get_picture
    working so far

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,32 +1,38 @@
+
 class Rectangle:
     
     def __init__(self, width, height):
-        pass
+        self.width = width
+        self.height = height
 
-    def set_width(self):
-        pass
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
 
-    def set_height(self):
-        pass
+    def set_width(self, width):
+        self.width=width
+
+    def set_height(self, height):
+        self.height = height
     
     def get_area(self):
-        pass
-        #area = width*height
-        #return area
+        area = self.width*self.height
+        return area
     
     def get_perimeter(self):
-        #perimeter = 2*width + 2*height
-        pass
-        #return perimeter
+        perimeter = 2*self.width + 2*self.height
+        return perimeter
     
     def get_diagonal(self):
-        #diagonal = ((width ** 2 + height ** 2) ** .5)
-        pass
-        #return diagonal
+        diagonal = ((self.width ** 2 + self.height ** 2) ** .5)
+        return diagonal
     
     def get_picture(self):
-        pass
-
+        if self.width > 50 or self.height > 50:
+            return 'Too big for picture.'
+        else:
+            picture = ("*"*self.width + "\n")*self.height
+            return picture
+    
     def get_amount_inside(self):
         pass
 
